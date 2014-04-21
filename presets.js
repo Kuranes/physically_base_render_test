416cb69ae42b67defa4b9653031a16fae3c4cf03
tuan <tuan.kuranes@gmail.com>
2014-04-21 21:16:31 +0200

    - adds env brute force blurring. (should do that in a rendertexture or worker once and for all) - add John Hable optimised GGX (enabled but that breaks educationnal selectable others, have to update UI)

--- presets.js
+++ presets.js
@@ -1,113 +1,192 @@
-
-    // copy paste from dat.gui save mechanism
+   // copy paste from dat.gui save mechanism
     var presets = {
-        "preset": "Default",
-        "remembered": {
-            "Default": {
-                "0": {}
-            },
-            "Gun": {
-                "0": {
-                    "model": "gun",
-                    "Precision": "HIGH",
-                    "Albedo": [ 255, 200, 200, 1 ],
-                    "Specular": [ 0, 200, 200, 1 ],
-                    "Roughness": 0.7000000000000001,
-                    "Dielectric": 0.6000000000000001,
-                    "lightColor": [ 255, 255, 255, 1 ],
-                    "lightIntensity": 1,
-                    "gamma": 2.2,
-                    "metallicSpecularDefine": "METALLIC",
-                    "normalDistributionDefine": "GGX",
-                    "fresnelDefine": "SCHLICK",
-                    "geometryDefine": "SMITH_SCHLICK_GGX",
-                    "energyRatioDefine": "FresnelDiff",
-                    "roughnessGlossinessDefine": "ROUGHNESS",
-                    "specAA": "TOKSVIG",
-                    "DiffuseMap": "CERBERUS",
-                    "NormalMap": "CERBERUS",
-                    "RoughnessMap": "CERBERUS",
-                    "MetallicMap": "CERBERUS",
-                    "EnvMap": "GrandCanyon_C_YumaPoint"
-                }
-            },
-            "MarbleTest": {
-                "0": {
-                    "model": "materialTest",
-                    "Precision": "HIGH",
-                    "Albedo": [ 220, 17.254901960784323, 17.254901960784323, 1 ],
-                    "Specular": [ 0, 200, 200, 1 ],
-                    "Roughness": 0.7000000000000001,
-                    "Dielectric": 0.6000000000000001,
-                    "lightColor": [ 232.5, 225.79584775086502, 118.52941176470587, 1 ],
-                    "lightIntensity": 0.4,
-                    "gamma": 2.4000000000000004,
-                    "metallicSpecularDefine": "METALLIC",
-                    "normalDistributionDefine": "GGX",
-                    "fresnelDefine": "SCHLICK",
-                    "geometryDefine": "SMITH_SCHLICK_GGX",
-                    "energyRatioDefine": "FresnelDiff",
-                    "roughnessGlossinessDefine": "ROUGHNESS",
-                    "specAA": "TOKSVIG",
-                    "DiffuseMap": "BASE",
-                    "NormalMap": "BASE",
-                    "RoughnessMap": "BASE",
-                    "MetallicMap": "BASE",
-                    "EnvMap": "Alexs_Apartment"
-                }
-            },
-            "ogre": {
-                "0": {
-                    "model": "ogre",
-                    "Precision": "HIGH",
-                    "Albedo": [ 11.176470588235297, 95, 0, 1 ],
-                    "Specular": "#94a03a",
-                    "Roughness": 0.7000000000000001,
-                    "Dielectric": 0.4,
-                    "lightColor": [ 232.5, 225.79584775086502, 118.52941176470587, 1 ],
-                    "lightIntensity": 0.4,
-                    "gamma": 2.4000000000000004,
-                    "metallicSpecularDefine": "SPECULAR",
-                    "normalDistributionDefine": "BLINNPHONG",
-                    "fresnelDefine": "SCHLICK",
-                    "geometryDefine": "SMITH_SCHLICK_GGX",
-                    "energyRatioDefine": "FresnelDiff",
-                    "roughnessGlossinessDefine": "ROUGHNESS",
-                    "specAA": "TOKSVIG",
-                    "DiffuseMap": "NONE",
-                    "NormalMap": "NONE",
-                    "RoughnessMap": "NONE",
-                    "MetallicMap": "NONE",
-                    "EnvMap": "Arches_E_PineTree"
-                }
-            }
-        },
-        "closed": false,
-        "folders": {
-            "Colors": {
-                "preset": "Default",
-                "closed": false,
-                "folders": {}
-            },
-            "PBR": {
-                "preset": "Default",
-                "closed": false,
-                "folders": {}
-            },
-            "light": {
-                "preset": "Default",
-                "closed": false,
-                "folders": {}
-            },
-            "Equations": {
-                "preset": "Default",
-                "closed": false,
-                "folders": {}
-            },
-            "Texture": {
-                "preset": "Default",
-                "closed": false,
-                "folders": {}
-            }
-        }
-    };
+  "preset": "Default",
+  "remembered": {
+    "Default": {
+      "0": {
+        "model": "materialTest",
+        "Precision": "HIGH",
+        "Albedo": [
+          255,
+          254.99999999999994,
+          0,
+          1
+        ],
+        "Specular": "#ffffff",
+        "Roughness": 1,
+        "Dielectric": 1,
+        "lightColor": [
+          235.00000000000003,
+          235.00000000000003,
+          235.00000000000003,
+          1
+        ],
+        "lightIntensity": 2.5,
+        "lightAmbientIntensity": 0.5,
+        "gamma": 2.2,
+        "exposure": 0,
+        "metallicSpecularDefine": "METALLIC",
+        "normalDistributionDefine": "BLINNPHONG",
+        "fresnelDefine": "SCHLICK",
+        "geometryDefine": "SMITH_SCHLICK_GGX",
+        "energyRatioDefine": "FresnelDiff",
+        "roughnessGlossinessDefine": "ROUGHNESS",
+        "specAA": "TOKSVIG",
+        "DiffuseMap": "NONE",
+        "NormalMap": "BASE",
+        "RoughnessMap": "NONE",
+        "MetallicMap": "NONE",
+        "EnvMap": "Milkyway"
+      }
+    },
+    "Gun": {
+      "0": {
+        "model": "gun",
+        "Precision": "HIGH",
+        "Albedo": [
+          255,
+          200,
+          200,
+          1
+        ],
+        "Specular": [
+          0,
+          200,
+          200,
+          1
+        ],
+        "Roughness": 0.6000000000000001,
+        "Dielectric": 1,
+        "lightColor": [
+          255,
+          255,
+          255,
+          1
+        ],
+        "lightIntensity": 8.3,
+        "lightAmbientIntensity": 1,
+        "gamma": 2.2,
+        "exposure": 0,
+        "metallicSpecularDefine": "METALLIC",
+        "normalDistributionDefine": "GGX",
+        "fresnelDefine": "SCHLICK",
+        "geometryDefine": "SMITH_SCHLICK_GGX",
+        "energyRatioDefine": "FresnelDiff",
+        "roughnessGlossinessDefine": "ROUGHNESS",
+        "specAA": "TOKSVIG",
+        "DiffuseMap": "CERBERUS",
+        "NormalMap": "CERBERUS",
+        "RoughnessMap": "CERBERUS",
+        "MetallicMap": "CERBERUS",
+        "EnvMap": "GrandCanyon_C_YumaPoint"
+      }
+    },
+    "MarbleTest": {
+      "0": {
+        "model": "materialTest",
+        "Precision": "HIGH",
+        "Albedo": [
+          220,
+          17.254901960784323,
+          17.254901960784323,
+          1
+        ],
+        "Specular": [
+          0,
+          200,
+          200,
+          1
+        ],
+        "Roughness": 0.8,
+        "Dielectric": 0.2,
+        "lightColor": [
+          232.5,
+          225.79584775086502,
+          118.52941176470587,
+          1
+        ],
+        "lightIntensity": 2.1,
+        "lightAmbientIntensity": 0.1,
+        "gamma": 2.4000000000000004,
+        "exposure": 0,
+        "metallicSpecularDefine": "METALLIC",
+        "normalDistributionDefine": "GGX",
+        "fresnelDefine": "SCHLICK",
+        "geometryDefine": "SMITH_SCHLICK_GGX",
+        "energyRatioDefine": "FresnelDiff",
+        "roughnessGlossinessDefine": "ROUGHNESS",
+        "specAA": "TOKSVIG",
+        "DiffuseMap": "BASE",
+        "NormalMap": "BASE",
+        "RoughnessMap": "BASE",
+        "MetallicMap": "BASE",
+        "EnvMap": "Alexs_Apartment"
+      }
+    },
+    "ogre": {
+      "0": {
+        "model": "ogre",
+        "Precision": "HIGH",
+        "Albedo": [
+          11.176470588235297,
+          95,
+          0,
+          1
+        ],
+        "Specular": "#94a03a",
+        "Roughness": 1,
+        "Dielectric": 0,
+        "lightColor": [
+          232.5,
+          225.79584775086502,
+          118.52941176470587,
+          1
+        ],
+        "lightIntensity": 2.4000000000000004,
+        "lightAmbientIntensity": 0.30000000000000004,
+        "gamma": 2.2,
+        "exposure": 0,
+        "metallicSpecularDefine": "METALLIC",
+        "normalDistributionDefine": "BLINNPHONG",
+        "fresnelDefine": "SCHLICK",
+        "geometryDefine": "SMITH_SCHLICK_GGX",
+        "energyRatioDefine": "FresnelDiff",
+        "roughnessGlossinessDefine": "ROUGHNESS",
+        "specAA": "NONE",
+        "DiffuseMap": "NONE",
+        "NormalMap": "NONE",
+        "RoughnessMap": "NONE",
+        "MetallicMap": "NONE",
+        "EnvMap": "Arches_E_PineTree"
+      }
+    }
+  },
+  "closed": false,
+  "folders": {
+    "Colors": {
+      "preset": "Default",
+      "closed": true,
+      "folders": {}
+    },
+    "PBR": {
+      "preset": "Default",
+      "closed": true,
+      "folders": {}
+    },
+    "light": {
+      "preset": "Default",
+      "closed": true,
+      "folders": {}
+    },
+    "Equations": {
+      "preset": "Default",
+      "closed": false,
+      "folders": {}
+    },
+    "Texture": {
+      "preset": "Default",
+      "closed": true,
+      "folders": {}
+    }
+  }
+};
